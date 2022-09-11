02c8da354bd1c42d5cafd27fb401c4f255f4de94
davronkhamdamov <xamdamovdavron6@gmail.com>
2022-09-11 18:18:26 +0500

    js

--- main.js
+++ main.js
@@ -1,11 +1,52 @@
-let time = Number(prompt('kun boshidam  qancha sekund otdi'));
-function name1(time) {
-  let x = time / 60;
-  let y = time / 3600;
-  let result1 = x.toString();
-  let result2 = y.toString();
-  let result = `Kun boshidan ${result1} minut o'tdi:
-Kun boshidan  ${result2} soat o'tdi`;
-  return result;
-}
-alert(name1(time));
+// let time = Number(prompt('kun boshidam  qancha sekund otdi'));
+// function name1(time) {
+//   let x = time / 60;
+//   let y = time / 3600;
+//   let result1 = x.toString();
+//   let result2 = y.toString();
+//   let result = `Kun boshidan ${result1} minut o'tdi:
+// Kun boshidan  ${result2} soat o'tdi`;
+//   return result;
+// }
+
+// alert(name1(time));
+// let x = 12;
+// let y = x.toString();
+// let z = y.split('');
+// function name1(a) {
+//   let b = Number(a[0]),
+//     c = Number(a[1]);
+//   let result = b + c;
+//   return result;
+// }
+// console.log(name1(z));
+
+// let x = 12;
+// let y = x.toString();
+// let z = y.split('');
+// function name1(a) {
+//   let b = Number(a[0]),
+//     c = Number(a[1]);
+//   let result = b * c;
+//   return result;
+// }
+// console.log(name1(z));
+
+// let son = prompt('son kiriting');
+// let number = Number(son);
+// let hisobla = son / 100;
+// console.log(parseInt(hisobla));
+// let age = prompt('son-1');
+// let age2 = prompt('son-2');
+// function name(a, b) {
+//   let aniq = a > 999;
+//   console.log(aniq);
+//   let aniq2 = b > 999;
+//   console.log(aniq2);
+//   let hisobla1 = a / 100;
+//   console.log(`Birinchi prompt yuzlar honasi = ${Math.floor(hisobla1)}`);
+//   let hisobla2 = b / 100;
+//   console.log(`Ikkinchi promptning yuzlar honasi = ${Math.floor(hisobla2)}`);
+//   return hisobla1, hisobla2;
+// }
+// name(age, age2);
